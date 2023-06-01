69324733df61bebcfaff2dffce9ada713dc2cc94
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-06-01 17:05:15 +0100

    commit

--- backend/Healthconnect/Healthconnect/settings.py
+++ backend/Healthconnect/Healthconnect/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
