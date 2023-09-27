30ef3925de0fb82580fcde6c490071326218c7ae
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-09-27 20:42:58 +0100

    commit

--- backend/Healthconnect/hcb/models.py
+++ backend/Healthconnect/hcb/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.utils import timezone
 import uuid
 
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
@@ -22,13 +23,14 @@ class UserManager(BaseUserManager):
 
 
 User_Roles = (
-    ('PATIENT','PATIENT'),
-    ('DOCTOR','DOCTOR'),
+    ('PATIENT', 'PATIENT'),
+    ('DOCTOR', 'DOCTOR'),
 )
 User_Gender = (
-    ('MALE','MALE'),
-    ('FEMALE','FEMALE'),
+    ('MALE', 'MALE'),
+    ('FEMALE', 'FEMALE'),
 )
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
@@ -38,29 +40,33 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    role= models.CharField(max_length=30,choices=User_Roles)
-    country = models.CharField(max_length=20,null=True, blank=True)
-    state=models.CharField(max_length=20,null=True, blank=True)
-    image= models.ImageField(upload_to='users', null=True,blank=True,default='default.jpg')
-    phone_number = models.CharField(max_length=20 , blank=True,null=True)
-    gender = models.CharField(max_length=10, choices=User_Gender,null=True,blank=True)
+    role = models.CharField(max_length=30, choices=User_Roles)
+    country = models.CharField(max_length=20, null=True, blank=True)
+    state = models.CharField(max_length=20, null=True, blank=True)
+    image = models.ImageField(
+        upload_to='users', null=True, blank=True, default='default.jpg')
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(
+        max_length=10, choices=User_Gender, null=True, blank=True)
 
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     objects = UserManager()
-    
+
     @property
     def fullname(self):
         return f'{self.first_name} {self.last_name}'
-              
+
     def __str__(self):
         return self.email
+
     def has_perm(self, perm: str, obj: None = None) -> bool:
         return super().has_perm(perm, obj)
-    
+
     def has_module_perms(self, app_label: str) -> bool:
         return super().has_module_perms(app_label)
+
 
 Patient_MaritalStatus = (
     ('SINGLE', 'SINGLE'),
@@ -68,113 +74,124 @@ Patient_MaritalStatus = (
     ('DIVORCED', 'DIVORCED'),
 )
 
+
 class Patient (models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    user=models.OneToOneField(User, on_delete=models.CASCADE,related_name='patient')
-    blood_group=models.CharField(max_length=30,null=True,blank=True)
-    genotype=models.CharField(max_length=30,null=True,blank=True)
-    weight=models.FloatField(null=True,blank=True)
-    age=models.IntegerField(null=True, blank=True)
-    marital_status=models.CharField(max_length=20, choices=Patient_MaritalStatus, null=True, blank=True)
-    medical_history=models.TextField(blank=True, null=True)
-    
-    def appointments(self,status=None):
-        if status != None:  
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='patient')
+    blood_group = models.CharField(max_length=30, null=True, blank=True)
+    genotype = models.CharField(max_length=30, null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    marital_status = models.CharField(
+        max_length=20, choices=Patient_MaritalStatus, null=True, blank=True)
+    medical_history = models.TextField(blank=True, null=True)
+
+    def appointments(self, status=None):
+        if status != None:
             appointment = self.patient_appointment.filter(status=status)
             return appointment
         else:
             appointment = self.patient_appointment.all()
             return appointment
-        
+
     def isPatientComplete(self):
-        if((self.blood_group is None) &(self.genotype is None) & 
-           (self.weight is None) & (self.age is None) ):
+        if ((self.blood_group is None) & (self.genotype is None) &
+           (self.weight is None) & (self.age is None)):
             return False
         return True
-        
+
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
-    
-    
+
+
 class Specialization(models.Model):
     name = models.CharField(max_length=20)
-    
+
     def __str__(self):
         return self.name
+
 
 class Doctor(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    user= models.OneToOneField(User, on_delete=models.CASCADE,related_name='doctor')
-    hospital=models.CharField(max_length=50,null=True,blank=True)
-    experience=models.IntegerField(null=True,blank=True)
-    field = models.CharField( max_length=50, null=True,blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='doctor')
+    hospital = models.CharField(max_length=50, null=True, blank=True)
+    experience = models.IntegerField(null=True, blank=True)
+    field = models.CharField(max_length=50, null=True, blank=True)
     bio = models.CharField(max_length=50, null=True, blank=True)
-    qualification = models.CharField(max_length=50, null=True,blank=True)
-    location = models.CharField(max_length=50, null=True,blank=True)
-    price = models.PositiveBigIntegerField(null=True,blank=True)
-    
-    
-    def appointments(self,status=None):
-        if status != None:  
-            appointment = self.doctor_appointment.filter(status=status).order_by('date')
+    qualification = models.CharField(max_length=50, null=True, blank=True)
+    location = models.CharField(max_length=50, null=True, blank=True)
+    price = models.PositiveBigIntegerField(null=True, blank=True)
+
+    def appointments(self, status=None):
+        if status != None:
+            appointment = self.doctor_appointment.filter(
+                status=status).order_by('date')
             return appointment
         else:
             appointment = self.doctor_appointment.all().order_by('date')
             return appointment
-        
-    @property    
+
+    @property
     def reviews(self):
-        reviews=self.doctor_reviews.all()
+        reviews = self.doctor_reviews.all()
         return reviews
-        
-    
+
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
-    
+
 
 AppointmentStatus = (
     ('PENDING', 'PENDING'),
     ('DONE', 'DONE'),
     ('CANCELLED', 'CANCELLED'),
 )
-    
+
+
 class Appointment(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
-    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL,null=True,related_name='doctor_appointment')
-    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL,null=True,related_name='patient_appointment')
-    status = models.CharField(max_length=10,choices=AppointmentStatus, default='PENDING', null=True, blank=True)
-    date = models.CharField(max_length=20, blank=True,null=True)
-    time = models.CharField(max_length=10, blank=True,null=True)
-    fee = models.PositiveBigIntegerField(null=True,blank=True)
-    
-    
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.SET_NULL, null=True, related_name='doctor_appointment')
+    patient = models.ForeignKey(
+        Patient, on_delete=models.SET_NULL, null=True, related_name='patient_appointment')
+    status = models.CharField(
+        max_length=10, choices=AppointmentStatus, default='PENDING', null=True, blank=True)
+    date = models.CharField(max_length=20, blank=True, null=True)
+    time = models.CharField(max_length=10, blank=True, null=True)
+    fee = models.PositiveBigIntegerField(null=True, blank=True)
+
     def __str__(self) -> str:
         return f'{self.id}'
-    
+
+
 class Review(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
-    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL,null=True,related_name='doctor_reviews')
-    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL,null=True,related_name='patient_reviews')
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.SET_NULL, null=True, related_name='doctor_reviews')
+    patient = models.ForeignKey(
+        Patient, on_delete=models.SET_NULL, null=True, related_name='patient_reviews')
     content = models.TextField(blank=True, null=True)
     date = models.DateField(auto_now=True)
-    
+
     def __str__(self):
         return f'{self.date} {self.doctor.user.first_name}'
-    
-    
+
+
 class Blog(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='blog', null=True,blank=True,default='default.jpg')
+    image = models.ImageField(
+        upload_to='blog', null=True, blank=True, default='default.jpg')
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    
+
     def __str__(self):
         return f'{self.title}'
-    
-    
